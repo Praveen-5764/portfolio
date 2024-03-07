6cbbcded093f54d793783c606e3232fa86866389
Praveen-AP <148032908+Praveen-5764@users.noreply.github.com>
2024-03-08 01:03:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function menuClick() {
+    console.log("TEST");
+    var menu= document.getElementById('mobileMenu');
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+}
